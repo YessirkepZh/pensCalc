3600ad8d9d2e58a53e9b5e0f763a90ff01ab50fa
Yessirkep <esik.95.kz@gmail.com>
2020-05-27 11:23:22 +0600

    Updates

---
+++ precache-manifest.62bc2f2307878f36cba940c1374e2295.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36fd5667d745b24d157ce95af1594c46",
+    "url": "/pensCalc/index.html"
+  },
+  {
+    "revision": "0a65526c17f68b4a24cd",
+    "url": "/pensCalc/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "5e6e4eb69c696e503ad8",
+    "url": "/pensCalc/static/css/main.e57ba918.chunk.css"
+  },
+  {
+    "revision": "0a65526c17f68b4a24cd",
+    "url": "/pensCalc/static/js/2.2800acdf.chunk.js"
+  },
+  {
+    "revision": "f70ed24c7e3911447d378b22f62dae26",
+    "url": "/pensCalc/static/js/2.2800acdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6e4eb69c696e503ad8",
+    "url": "/pensCalc/static/js/main.46b5d35c.chunk.js"
+  },
+  {
+    "revision": "8efd6cd6ba14f10b8195",
+    "url": "/pensCalc/static/js/runtime-main.8ab5360b.js"
+  }
+]);
